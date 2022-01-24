6b4bd4a1b37870a5fab61bd163ac55764cd4ae5c
Matheus Vargem <matheusvrgm@gmail.com>
2022-01-24 00:37:36 -0300

    improve function

--- apiLambda/index.js
+++ apiLambda/index.js
@@ -1,24 +1,20 @@
-const {
-	DynamoDBClient,
-	PutItemCommand,
-	PutItemInput,
-} = require("@aws-sdk/client-dynamodb"); //
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb"); //
 
 exports.handler = async (event) => {
-	const { name, email } = event;
+  //const { name, email } = event.body; <- Use without HTTP GW
+  const { name, email } = JSON.parse(event.body); //<- Use WITH HTTP GW
 
-	const client = new DynamoDBClient({
-		region: "us-east-1",
-		apiVersion: "2012-08-10",
-	});
+  const client = new DynamoDBClient({
+    region: process.env.REGION_NAME,
+    apiVersion: "2012-08-10",
+  });
 
-	const command = new PutItemCommand({
-		TableName: "nossatabela",
-		Item: { name: { S: name }, email: { S: email } },
-	});
+  const command = new PutItemCommand({
+    TableName: process.env.TABLE_NAME,
+    Item: { name: { S: name }, email: { S: email } },
+  });
 
-	const response = await client.send(command);
+  const response = await client.send(command);
 
-	return response;
+  return response;
 };
-
